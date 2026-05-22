81bd1c845adfb94875dca268d29b384860e4daf5
Shreyansh <kake0shreyansh@gmail.com>
2026-05-22 17:45:10 +0530

    update

---
+++ backend/routes/logo.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../uploads/logo/app-logo.png'));
+});
+
+module.exports = router;
